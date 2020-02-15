c0832feffdf14971ae8ebf45bd172ddb221bdbf5
aakunitsa <28026247+aakunitsa@users.noreply.github.com>
2020-02-14 20:58:20 -0600

    removed some tests; fci for aux is still broken..

--- testing/simple_fci_li2.cpp
+++ testing/simple_fci_li2.cpp
@@ -1,24 +1,24 @@
 #include <iostream>
 #include <cstdio>
+#include "ref_qhamiltonian.h"
 #include "qhamiltonian_mpi.h"
+#include "qparams_reader.h"
 #include "qintegral.h"
 #include "qorbitals.h"
-#include "qparams_reader.h"
 #include "qbasis.h"
 #include <map>
 #include <string>
 #include <cassert>
 #include <vector>
-
 #include <mpi.h>
 
 // This file is for debugging
 
 int main(int argc, char **argv) {
 
-	MPI_Init(&argc, &argv);
-	int me;
-	MPI_Comm_rank(MPI_COMM_WORLD, &me);
+    MPI_Init(&argc, &argv);
+    int me;
+    MPI_Comm_rank(MPI_COMM_WORLD, &me);
     double thresh = 1e-10;
     double e_from_hande = -7.448837899249; // Would only be valid for Li 2-S with L = 0 and radial grid of 25 points
     Params_reader q(argc, argv);
@@ -28,11 +28,11 @@ int main(int argc, char **argv) {
     DetBasis bas(q.params, ig.n1porb);
     //REF::DetBasis ref_bas(q.params, ig.n1porb);
     //REF::Hamiltonian h_ref(ig, ref_bas);
-	Hamiltonian_mpi h(ig, bas);
+    Hamiltonian_mpi h(ig, bas);
     auto e_ = h.diag();
     if (me == 0) printf("Ground state of Li 2-S (%d pt radial grid / L = %d) : %20.12f\n", q.params["nrad"], q.params["L_max"], e_[0]);
     assert( abs(e_from_hande - e_[0]) <= thresh );
-	MPI_Finalize();
+    MPI_Finalize();
     return 0;
 }
 
